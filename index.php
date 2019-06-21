02bc3815f9eca4cdc8b11df72369a424ef94916f
Brice WILLIAM <brice.william971@gmail.com>
2019-06-21 15:53:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!file_exists(__DIR__.'/vendor/autoload.php')) {
+    throw new Exception('Exec composer install');
+}
+require __DIR__.'/vendor/autoload.php';
+
+?>
+<div class="titre">
+   <h1>Gestion des meeting</h1>
+</div>
